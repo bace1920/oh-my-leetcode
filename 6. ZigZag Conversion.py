496ff1d1c71cc843fd0f2cd27a1a11ed53b3e775
bluehead <yiyang.chen@xiyanghui.com>
2018-09-10 22:22:07 +0800

    problem 6 zigzag conversion

---
+++ 6. ZigZag Conversion.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    def convert(self, s, numRows):
+        """
+        we can treat the ZipZag of like follow matrix
+        1   7    14
+        2 4 8 13 15
+        3 5 9 12
+          6   11
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        length: int = len(s)
+        if (numRows == 1) or length <= numRows:
+            return s
+        groupSize: int = numRows - 1
+        groupNum: int = length // groupSize if length // groupSize == 0 else length // groupSize + 1
+        result = ''
+        i = 0
+        while i < (groupSize + 1) * groupNum:
+            y = i // groupNum
+            x = i - y * groupNum
+            if (x % 2 == 0 and y == numRows - 1) or (x % 2 == 1 and y == 0):
+                # these coordinate on the matrix is empty
+                pass
+            else:
+                index = x * groupSize + ((numRows - y - 1 ) if x % 2 else y)
+                if index < length:
+                    result += s[index]
+            i += 1
+        return result
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.convert('PAYPALISHIRING', 3))
