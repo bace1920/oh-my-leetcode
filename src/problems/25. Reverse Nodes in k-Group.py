417cab6e46ca1c592f2d33ce49a679e4f8b6c0e6
bluehead <yiyang.chen@xiyanghui.com>
2018-09-19 23:54:51 +0800

    update 25 k group reverse

---
+++ src/problems/25. Reverse Nodes in k-Group.py
@@ -0,0 +1,61 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        if not head or k ==1:
+            return head
+        new_head = head
+        if self.isEnough(new_head, k):
+            now_head, now_last, next_head = self.reverseK(head, k)
+            new_head = now_head
+            now_last.next = next_head
+            last_last = now_last
+            while self.isEnough(last_last.next, k):
+                now_head, now_last, next_head = self.reverseK(next_head, k)
+                last_last.next = now_head
+                now_last.next = next_head
+                last_last = now_last
+        return new_head
+
+    def isEnough(self, now_head, k):
+        tmp = now_head
+        for i in range(k):
+            if not tmp:
+                return False
+            tmp = tmp.next
+        return True
+
+    def reverseK(self, head, k):
+        now = head.next
+        prev = head
+        nex = now.next
+        count = 0
+        while count < k - 1:
+            now.next = prev
+            prev = now
+            now = nex
+            nex = now.next if now else None
+            count += 1
+        return prev, head, now
+
+
+if __name__ == '__main__':
+    now = head = ListNode(1)
+    for i in range(2, 6):
+        now.next = ListNode(i)
+        now = now.next
+
+    solu = Solution()
+    head = solu.reverseKGroup(head, 2)
+    while head is not None:
+        print(head.val)
+        head = head.next
